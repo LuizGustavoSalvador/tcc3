9948d83fb0258391fe62995cb5b894baeb3ed73e
Luiz Gustavo Salvador <luiz.gustavo@nexello.com.br>
2024-05-14 00:18:07 -0300

    ajustes

--- moodle/local/gamificationhelper/lib.php
+++ moodle/local/gamificationhelper/lib.php
@@ -2,32 +2,35 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-function local_gamificationhelper_extend_settings_navigation($settingsnav, $context) {
+function local_gamificationhelper_extend_settings_navigation(settings_navigation $settingsnav, $context) {
     global $CFG, $PAGE;
 
-    if (!$PAGE->course or $PAGE->course->id == 1) {
-        return;
+    // Verifica se estamos dentro de um curso que não seja a página principal do site
+    if ($PAGE->course->id == 1) {
+        return;  // Não adiciona o link no contexto do site principal
     }
 
-    if (!has_capability('local/gamificationhelper:view', context_course::instance($PAGE->course->id))) {
-        return;
+    // Verifica se o usuário tem a capacidade de visualizar o plugin no contexto atual do curso
+    if (!has_capability('local/gamificationhelper:view', $context)) {
+        return;  // O usuário não tem permissão para ver o plugin, retorna sem adicionar ao menu
     }
 
+    // Localiza o nó de administração do curso no menu de configurações
     if ($settingnode = $settingsnav->find('courseadmin', navigation_node::TYPE_COURSE)) {
         $strgamificationhelper = get_string('pluginname', 'local_gamificationhelper');
-        $url = new moodle_url('/local/gamificationhelper/index.php', array('id' => $PAGE->course->id));
+        $url = new moodle_url('/local/gamificationhelper/index.php', ['id' => $PAGE->course->id]);
+
+        // Cria um nó de navegação para o plugin
         $gamificationhelpernode = navigation_node::create(
             $strgamificationhelper,
             $url,
             navigation_node::NODETYPE_LEAF,
-            'pluginname',
-            'pluginname',
+            'gamificationhelper',
+            'gamificationhelper',
             new pix_icon('i/settings', $strgamificationhelper)
         );
-        // Marcar como ativo se estivermos na página correspondente
-        if ($PAGE->url->compare($url, URL_MATCH_BASE)) {
-            $gamificationhelpernode->make_active();
-        }
+
+        // Adiciona o nó ao menu de administração do curso
         $settingnode->add_node($gamificationhelpernode);
     }
 }
